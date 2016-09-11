d086d92686e32f32013d8033afd63723f8a3d3f3
Dmitry Pavlov <dpavlov@dmitrys-mbp.corp.cyren.com>
2016-09-11 10:58:01 +0300

    add start all script

---
+++ start-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd spark4kube  
+#Run: sh start-all.sh it should start all the scripts, or as alternative run line by line:  
+echo "Create master instance"
+kubectl create -f spark-master.yaml   
+echo "Create two services so workers can find the master and you can access the gui"  
+kubectl create -f spark-master-service.yaml  
+kubectl create -f spark-webui-service.yaml  
+echo "Validate that service is available on "  
+echo $(http://$(hostname -i):8080/api/v1/proxy/namespaces/default/services/spark-webui/)
+#Run slaves, to change number os slaves open spark-slaves.yaml script and change number of pods from 3 to any number.  
+kubectl create -f spark-slaves.yaml  
