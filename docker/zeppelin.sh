734145d5eb2762723bb7f24fbac7e11a6de2c42e
root <root@dwhlab01.corp.cyren.com>
2016-09-21 12:42:16 +0300

    ...

--- docker/zeppelin.sh
+++ docker/zeppelin.sh
@@ -8,7 +8,6 @@ cp zeppelin-env.sh.template zeppelin-env.sh
 echo 'export MASTER=spark://spark-master:7077'>>zeppelin-env.sh
 echo 'export ZEPPELIN_NOTEBOOK_STORAGE="org.apache.zeppelin.notebook.repo.VFSNotebookRepo, org.apache.zeppelin.notebook.repo.zeppelinhub.ZeppelinHubRepo"'>>zeppelin-env.sh
 echo 'export ZEPPELINHUB_API_ADDRESS="https://www.zeppelinhub.com"'>>zeppelin-env.sh
-echo 'export ZEPPELINHUB_API_ADDRESS="https://www.zeppelinhub.com"'>>zeppelin-env.sh
 echo 'export ZEPPELIN_PORT=9995'>>zeppelin-env.sh
 cd ../bin/
 ./zeppelin-daemon.sh start
