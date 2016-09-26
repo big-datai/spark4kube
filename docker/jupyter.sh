9e1e826e3d62a987b5c4b24df1fa9730983277ac
root <root@dwhlab01.corp.cyren.com>
2016-09-26 18:50:16 +0300

    added jupyter file

--- docker/jupyter.sh
+++ docker/jupyter.sh
@@ -1,4 +1,6 @@
-echo 'c.NotebookApp.ip = '$(hostname -i)>>/root/.jupyter/jupyter_notebook_config.py
+#!/bin/sh
 
-PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser" pyspark --packages com.databricks:spark-csv_2.10:1.1.0,com.amazonaws:aws-java-sdk-pom:1.11.37,org.apache.hadoop:hadoop-aws:2.7.0 --master spark://spark-master:7077 >>/root/logs.jupyter &
-tail -F >>tail -F /usr/share/spark/logs/*
+
+echo 'c.NotebookApp.ip = "'"$(hostname -i)"'"'>>/root/.jupyter/jupyter_notebook_config.py
+PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser" pyspark --packages com.databricks:spark-csv_2.10:1.1.0,com.amazonaws:aws-java-sdk:1.7.4,org.apache.hadoop:hadoop-aws:2.7.1 --master spark://spark-master:7077 >>/usr/share/logs.jupyter&
+tail -F /usr/share/logs.jupyter
