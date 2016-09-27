1c3577311305e55862b90e7176f517a998a5db68
root <root@dwhlab01.corp.cyren.com>
2016-09-27 11:05:45 +0300

    added jupyter not as a service

--- docker/jupyter.sh
+++ docker/jupyter.sh
@@ -2,5 +2,5 @@
 
 
 echo 'c.NotebookApp.ip = "'"$(hostname -i)"'"'>>/root/.jupyter/jupyter_notebook_config.py
-PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser" pyspark --packages com.databricks:spark-csv_2.10:1.1.0,com.amazonaws:aws-java-sdk:1.7.4,org.apache.hadoop:hadoop-aws:2.7.1 --master spark://spark-master:7077 >>/usr/share/logs.jupyter&
-tail -F /usr/share/logs.jupyter
+PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser" pyspark --packages com.databricks:spark-csv_2.10:1.1.0,com.amazonaws:aws-java-sdk:1.7.4,org.apache.hadoop:hadoop-aws:2.7.1 --master spark://spark-master:7077 &
+tail -F /usr/share/spark/logs/*
