5f5f6f843beca2a21a1d5dd5fc2433a935173707
Dmitry <2dmitrypavlov@users.noreply.github.com>
2016-12-05 16:17:58 +0200

    Update zeppelin.sh

--- docker/zeppelin.sh
+++ docker/zeppelin.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #This script downloads latest zeppelin, installs and configures it to local spark://spark-master:7077
 
-wget http://apache.mivzakim.net/zeppelin/zeppelin-0.6.1/zeppelin-0.6.1-bin-all.tgz
-tar -zxvf zeppelin-0.6.1-bin-all.tgz
-cd zeppelin-0.6.1-bin-all/conf
+wget http://www-eu.apache.org/dist/zeppelin/zeppelin-0.6.2/zeppelin-0.6.2-bin-all.tgz
+tar -zxvf zeppelin-0.6.2-bin-all.tgz
+cd zeppelin-0.6.2-bin-all/conf
 cp zeppelin-env.sh.template zeppelin-env.sh
 echo 'export MASTER=spark://spark-master:7077'>>zeppelin-env.sh
-echo 'export ZEPPELIN_NOTEBOOK_STORAGE="org.apache.zeppelin.notebook.repo.VFSNotebookRepo, org.apache.zeppelin.notebook.repo.zeppelinhub.ZeppelinHubRepo"'>>zeppelin-env.sh
-echo 'export ZEPPELINHUB_API_ADDRESS="https://www.zeppelinhub.com"'>>zeppelin-env.sh
+#echo 'export ZEPPELIN_NOTEBOOK_STORAGE="org.apache.zeppelin.notebook.repo.VFSNotebookRepo, org.apache.zeppelin.notebook.repo.zeppelinhub.ZeppelinHubRepo"'>>zeppelin-env.sh
+#echo 'export ZEPPELINHUB_API_ADDRESS="https://www.zeppelinhub.com"'>>zeppelin-env.sh
 echo 'export ZEPPELIN_PORT=9995'>>zeppelin-env.sh
 cd ../bin/
 ./zeppelin-daemon.sh start
