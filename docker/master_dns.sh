31760aefbae6dbfbbf249f5cd233c34b6a562c49
Dmitry <2dmitrypavlov@users.noreply.github.com>
2017-01-17 16:43:25 +0200

    Update master_dns.sh

--- docker/master_dns.sh
+++ docker/master_dns.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 unset SPARK_MASTER_PORT
 cd /usr/share/spark
-bin/spark-class org.apache.spark.deploy.master.Master --ip spark-master.servicediscovery.internal
+SPARK_PUBLIC_DNS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4) bin/spark-class org.apache.spark.deploy.master.Master --ip spark-master.servicediscovery.internal
 
 # TODO: detect master exit
 tail -F /usr/share/spark/logs/*
