e60032e3b800f18707f38a756cc96407a3c3e1b1
Dmitry <2dmitrypavlov@users.noreply.github.com>
2017-01-23 23:04:11 +0200

    Create spark-env.sh

---
+++ spark-env.sh
@@ -0,0 +1,2 @@
+SPARK_WORKER_OPTS="-Dspark.worker.cleanup.enabled=true -Dspark.worker.cleanup.interval=10 -Dspark.worker.cleanup.appDataTtl=10"
+#spark.worker.cleanup.enabled=true spark.worker.cleanup.interval=10 spark.worker.cleanup.appDataTtl=10
