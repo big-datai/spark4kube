b4baf09ef3e7c7b8706a4edf07e2a9551afa8809
Dmitry Pavlov <dmitry@deepricer.com>
2017-05-26 16:56:00 +0300

    env

---
+++ docker/master_ip.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "$(hostname -i) spark-master" >>/etc/hosts
+unset SPARK_MASTER_PORT
+cd /usr/share/spark
+bin/spark-class org.apache.spark.deploy.master.Master --ip $MASTER
+
+# TODO: detect master exit
+tail -F /usr/share/spark/logs/*
