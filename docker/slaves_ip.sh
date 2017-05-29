de1eb57f6f657fabd7ad0cb359cdf26ea072b555
Dmitry <2dmitrypavlov@users.noreply.github.com>
2017-05-29 10:47:58 +0300

    Update slaves_ip.sh

--- docker/slaves_ip.sh
+++ docker/slaves_ip.sh
@@ -2,7 +2,7 @@
 
 cd /usr/share/spark
 cd /usr/local/spark
-bin/spark-class org.apache.spark.deploy.worker.Worker spark://$MASTER:7077 --webui-port 8081
+SPARK_PUBLIC_DNS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4) bin/spark-class org.apache.spark.deploy.worker.Worker spark://$MASTER:7077 --webui-port 8081
 
 # TODO: detect slave exit
 tail -F /usr/share/spark/logs/*
