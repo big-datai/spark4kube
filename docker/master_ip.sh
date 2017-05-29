b4b32fd01369271740310aedf42e990c255023d8
Dmitry <2dmitrypavlov@users.noreply.github.com>
2017-05-29 10:48:17 +0300

    Update master_ip.sh

--- docker/master_ip.sh
+++ docker/master_ip.sh
@@ -3,7 +3,7 @@
 echo "$(hostname -i) spark-master" >>/etc/hosts
 unset SPARK_MASTER_PORT
 cd /usr/share/spark
-bin/spark-class org.apache.spark.deploy.master.Master --ip $MASTER
+SPARK_PUBLIC_DNS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4) bin/spark-class org.apache.spark.deploy.master.Master --ip $MASTER
 
 # TODO: detect master exit
 tail -F /usr/share/spark/logs/*
