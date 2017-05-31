1cafbb3f1d28b761ddfd4a49fb12a6bd187c6db9
Dmitry <2dmitrypavlov@users.noreply.github.com>
2017-05-31 13:09:30 +0300

    Update zeppelin.sh

--- docker/zeppelin.sh
+++ docker/zeppelin.sh
@@ -8,7 +8,7 @@ wget https://github.com/s3tools/s3cmd/archive/master.zip
 
 wget http://apache-mirror.rbc.ru/pub/apache/zeppelin/zeppelin-0.7.1/zeppelin-0.7.1-bin-all.tgz
 tar -zxvf zeppelin-0.7.1-bin-all.tgz
-cd zeppelin-0.7.1-bin-all.tgz/conf
+cd zeppelin-0.7.1-bin-all/conf
 cp zeppelin-env.sh.template zeppelin-env.sh
 echo 'export MASTER=spark://'$MASTERZ':7077'>>zeppelin-env.sh
 echo 'export SPARK_SUBMIT_OPTIONS="--jars /usr/share/mysql-connector-java-5.1.39.jar,/usr/share/aws-java-sdk-1.7.4.jar,/usr/share/hadoop-aws-2.6.0.jar"'>>zeppelin-env.sh
@@ -18,4 +18,4 @@ echo 'export ZEPPELIN_PORT=9995'>>zeppelin-env.sh
 cd ../bin/
 ./zeppelin-daemon.sh start
 
-tail -F /usr/share/spark/logs/*
+tail -F /usr/share/zeppelin-0.7.1-bin-all/logs*
