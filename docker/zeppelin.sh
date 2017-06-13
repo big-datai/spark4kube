44fc74b1494e6c4f3dd64515cfa3e801f68f656c
Dmitry <2dmitrypavlov@users.noreply.github.com>
2017-06-13 18:27:18 +0300

    Create zeppelin.sh

--- docker/zeppelin.sh
+++ docker/zeppelin.sh
@@ -8,16 +8,16 @@ wget http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.39/mysql-con
 #wget https://github.com/s3tools/s3cmd/archive/master.zip
 
 cd /usr/share/
-wget http://archive.apache.org/dist/zeppelin/zeppelin-0.7.1/zeppelin-0.7.1-bin-all.tgz
-tar -zxvf zeppelin-0.7.1-bin-all.tgz
-cd zeppelin-0.7.1-bin-all/conf
-#cp zeppelin-env.sh.template zeppelin-env.sh
-#echo 'export MASTER=spark://'$MASTERZ':7077'>>zeppelin-env.sh
+wget http://archive.apache.org/dist/zeppelin/zeppelin-0.7.0/zeppelin-0.7.0-bin-all.tgz
+tar -zxvf zeppelin-0.7.0-bin-all.tgz
+cd zeppelin-0.7.0-bin-all/conf
+cp zeppelin-env.sh.template zeppelin-env.sh
+echo 'export MASTER=spark://'$MASTERZ':7077'>>zeppelin-env.sh
 #echo 'export SPARK_SUBMIT_OPTIONS="--jars /usr/share/mysql-connector-java-5.1.39.jar,/usr/share/aws-java-sdk-1.7.4.jar,/usr/share/hadoop-aws-2.6.0.jar"'>>zeppelin-env.sh
 #echo 'export ZEPPELIN_NOTEBOOK_STORAGE="org.apache.zeppelin.notebook.repo.VFSNotebookRepo, org.apache.zeppelin.notebook.repo.zeppelinhub.ZeppelinHubRepo"'>>zeppelin-env.sh
 #echo 'export ZEPPELINHUB_API_ADDRESS="https://www.zeppelinhub.com"'>>zeppelin-env.sh
 #echo 'export ZEPPELIN_PORT=9999'>>zeppelin-env.sh
-#echo 'export SPARK_HOME=/usr/share/spark'>>zeppelin-env.sh
+echo 'export SPARK_HOME=/usr/share/spark'>>zeppelin-env.sh
 cd ../bin/
 ./zeppelin-daemon.sh restart
 
