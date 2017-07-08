dc2b7ec14469fe7eb07ff538ef7ab1f8bbd0c273
Dmitry <2dmitrypavlov@users.noreply.github.com>
2017-07-08 19:39:17 +0300

    Update zeppelin.sh

--- docker/zeppelin.sh
+++ docker/zeppelin.sh
@@ -14,10 +14,11 @@ cd zeppelin-0.7.1-bin-all/conf
 cp zeppelin-env.sh.template zeppelin-env.sh
 echo 'export MASTER=spark://'$MASTERZ':7077'>>zeppelin-env.sh
 echo 'export SPARK_SUBMIT_OPTIONS="--jars /root/jars/mysql-connector-java-5.1.39.jar,/root/jars/aws-java-sdk-1.7.4.jar,/root/jars/hadoop-aws-2.6.0.jar"'>>zeppelin-env.sh
-#echo 'export ZEPPELIN_NOTEBOOK_STORAGE="org.apache.zeppelin.notebook.repo.VFSNotebookRepo, org.apache.zeppelin.notebook.repo.zeppelinhub.ZeppelinHubRepo"'>>zeppelin-env.sh
-#echo 'export ZEPPELINHUB_API_ADDRESS="https://www.zeppelinhub.com"'>>zeppelin-env.sh
+echo 'export ZEPPELIN_NOTEBOOK_STORAGE="org.apache.zeppelin.notebook.repo.VFSNotebookRepo, org.apache.zeppelin.notebook.repo.zeppelinhub.ZeppelinHubRepo"'>>zeppelin-env.sh
+echo 'export ZEPPELINHUB_API_ADDRESS="https://www.zeppelinhub.com"'>>zeppelin-env.sh
 echo 'export ZEPPELIN_PORT=9999'>>zeppelin-env.sh
 echo 'export SPARK_HOME=/usr/share/spark'>>zeppelin-env.sh
+
 cd ../bin/
 ./zeppelin-daemon.sh restart
 
