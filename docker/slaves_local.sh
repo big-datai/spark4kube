7d02036c0683978cb34422e36125103ea3acb315
Dmitry <2dmitrypavlov@users.noreply.github.com>
2017-07-18 22:15:33 +0300

    Delete slaves_local.sh

--- docker/slaves_local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /usr/share/spark
-echo "$(hostname -i) spark-master" >>/etc/hosts
-bin/spark-class org.apache.spark.deploy.worker.Worker spark://spark-master:7077 --webui-port 8081
-
-# TODO: detect slave exit
-tail -F /usr/share/spark/logs/*
