d93c8a4141d8601b607b14d141ec4390119d0cb8
Dmitry <2dmitrypavlov@users.noreply.github.com>
2017-07-18 22:14:53 +0300

    Delete master.sh

--- docker/master.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "$(hostname -i) spark-master" >>/etc/hosts
-unset SPARK_MASTER_PORT
-cd /usr/share/spark
-bin/spark-class org.apache.spark.deploy.master.Master --ip spark-master
-
-# TODO: detect master exit
-tail -F /usr/share/spark/logs/*
